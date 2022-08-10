5ed24fd60aa03f75d98d5ff3fa69ee44d1382161
Suhas Anandrao Kulkarni <100271877+maharaj2113@users.noreply.github.com>
2022-08-10 17:36:14 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/maharaj2113/Golang/router"
+)
+
+func main() {
+	fmt.Println("MongoDB API")
+	r := router.Router()
+	fmt.Println("Server is getting started")
+	log.Fatal(http.ListenAndServe(":4000", r))
+	fmt.Println("Listing to Port 4000")
+}
